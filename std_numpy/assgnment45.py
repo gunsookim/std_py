0f05ecfd5c5c5405ab5e77cf6137673f3df0aa97
gunsookim <soo85925@naver.com>
2022-11-22 15:49:17 +0900

    코드 수정

--- std_numpy/assgnment45.py
+++ std_numpy/assgnment45.py
@@ -1,9 +1,30 @@
 
 if __name__ == '__main__':
-    filename = input("파일명 입력: ")
+    dic = {'y': True, 'yes': True, 'n': False, 'no': False}
+    ans_repeat = True
+    while ans_repeat:
+        filename = input("파일명 입력: ")
+        with open(f"{filename}.txt", "rt") as f:
+            set_matrix = f.readline().split()
+            eq = set_matrix[0]
+            unk_v = set_matrix[1]
+            pos = f.tell()
+            rows = len(f.readlines())
+            f.seek(pos)
+            matrix = [list(map(str, f.readline().split())) for _ in range(rows)]
+            for i in range(rows):
+                matrix[i].insert(int(unk_v) - 1, '|')
+            for i in matrix:
+                for j in i:
+                    print(j, end=" ")
+                print()
+            ans_repeat = dic[input("반복여부 확인[y(yes) / n(no)]: ")]
+"""
+        while True:
+            ans_repeat = input("반복여부 확인[y(yes) / n(no)]: ")
+            if ans_repeat in dic:
+                ans_repeat = dic[ans_repeat]
+                break
+            else:
+                print("다시 입력해주세요.")"""
 
-    with open(f"{filename}.txt", "rt") as f:
-        row = len(f.readlines())
-        matrix = [list(map(int, f.readline().split())) for _ in range(row)]
-
-    print(matrix)
