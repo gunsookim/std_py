3b65d4ac25ddda89beea25bbe108688cd99162f7
gunsookim <soo85925@naver.com>
2022-11-23 21:00:16 +0900

    no message

--- std_numpy/assgnment45.py
+++ std_numpy/assgnment45.py
@@ -1,3 +1,6 @@
+def cal_rref(m):
+    return m
+
 
 if __name__ == '__main__':
     dic = {'y': True, 'yes': True, 'n': False, 'no': False}
@@ -12,13 +15,16 @@ if __name__ == '__main__':
             rows = len(f.readlines())
             f.seek(pos)
             matrix = [list(map(str, f.readline().split())) for _ in range(rows)]
+
+            #Augmented matrix 출력
             for i in range(rows):
                 matrix[i].insert(int(unk_v) - 1, '|')
             for i in matrix:
                 for j in i:
                     print(j, end=" ")
                 print()
-            ans_repeat = dic[input("반복여부 확인[y(yes) / n(no)]: ")]
+
+        ans_repeat = dic[input("반복여부 확인[y(yes) / n(no)]: ")]
 """
         while True:
             ans_repeat = input("반복여부 확인[y(yes) / n(no)]: ")
