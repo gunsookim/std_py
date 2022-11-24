86d1e784098ce87e074f7b16a06dd754cddc40c0
gunsookim <soo85925@naver.com>
2022-11-24 23:56:46 +0900

    정렬 코드 추가

--- std_numpy/assgnment45.py
+++ std_numpy/assgnment45.py
@@ -1,36 +1,54 @@
 def cal_rref(m):
-    return m
+    idx_row = list(range(len(m)))
+    idx_col = len(m[0]) - 1
+    m_pivot = [list(map(int, item[:])) for item in m]
+    ero = []
+    #   행렬 정렬
+    print(idx_row)
+    m_pivot.sort(key=lambda x: abs(x[0]), reverse=True)
+    """for c in range(idx_col):
+        nz_row = [r for r in idx_row if m_pivot[r][c] != 0]
+        if nz_row:
+            pivot = nz_row[0]
+            idx_row.remove(pivot)
+            ero.append(m_pivot[pivot])
+            for r in nz_row:
+                if r is not pivot:"""
+    print(f"ERO: {m_pivot}")
+    # for row in range(len(idx_row)):
 
 
 if __name__ == '__main__':
     dic = {'y': True, 'yes': True, 'n': False, 'no': False}
     ans_repeat = True
     while ans_repeat:
-        filename = input("파일명 입력: ")
-        with open(f"{filename}.txt", "rt") as f:
-            set_matrix = f.readline().split()
-            eq = set_matrix[0]
-            unk_v = set_matrix[1]
-            pos = f.tell()
-            rows = len(f.readlines())
-            f.seek(pos)
-            matrix = [list(map(str, f.readline().split())) for _ in range(rows)]
-
-            #Augmented matrix 출력
-            for i in range(rows):
-                matrix[i].insert(int(unk_v) - 1, '|')
-            for i in matrix:
-                for j in i:
-                    print(j, end=" ")
-                print()
-
-        ans_repeat = dic[input("반복여부 확인[y(yes) / n(no)]: ")]
-"""
         while True:
-            ans_repeat = input("반복여부 확인[y(yes) / n(no)]: ")
-            if ans_repeat in dic:
-                ans_repeat = dic[ans_repeat]
+            try:
+                filename = input("파일명 입력: ")
+                with open(f"{filename}.txt", "rt") as f:
+                    set_matrix = f.readline().split()
+                    eq = set_matrix[0]
+                    unk_v = set_matrix[1]
+                    print(f"Equation 수: {eq}, Unknown Vector 수: {unk_v}")
+                    pos = f.tell()
+                    rows = len(f.readlines())
+                    f.seek(pos)
+                    matrix = [list(map(str, f.readline().split())) for _ in range(rows)]
                 break
-            else:
-                print("다시 입력해주세요.")"""
-
+            except FileNotFoundError:
+                print("없는 파일입니다.")
+        aug_matrix = [item[:] for item in matrix]
+        #   Augmented matrix 출력
+        for i in range(rows):
+            aug_matrix[i].insert(int(unk_v) - 1, '|')
+        for i in aug_matrix:
+            for j in i:
+                print(j, end=" ")
+            print()
+        cal_rref(matrix)
+        while True:
+            try:
+                ans_repeat = dic[input("반복여부 확인[y(yes) / n(no)]: ")]
+                break
+            except KeyError:
+                print("다시 입력해주세요.")
